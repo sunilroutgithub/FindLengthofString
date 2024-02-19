151264cdad15b646334d561e21fe114b27474b0a
sunil_github <117855897+sunilroutgithub@users.noreply.github.com>
2024-02-19 18:20:46 +0530

    Add files via upload

---
+++ String.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string str ="sunilkumarrout";
+    int length =0;
+
+    for(int i=0; str[i] != 0;i++){
+        length++;
+    }
+   cout <<" The length of the string :" << length;
+   return 0; 
+}  
